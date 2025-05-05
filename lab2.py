1b909420fb00fdf807dc0660db9db91b5169c0bf
Yeo Song Heng <YEOSH.24@ichat.sp.edu.sg>
2025-05-05 10:24:10 +0800

    Inital version to display text message on console

--- lab2.py
+++ lab2.py
@@ -1,1 +1,24 @@
-print("ET0735 (DevOps for AIoT) - Lab 2 - Introduction to Python")
+def calculate_bmi(height, weight):
+    print("Height= "+ str(height))
+    print("Weight= "+ str(weight))
+    bmi = weight/(height**2)
+    print("BMI= "+str(bmi))
+
+    type = 0
+    if(bmi<18.5):
+        type = -1
+    if(18.5 <= bmi <= 25.0 ):
+        type = 0
+    if(bmi>25):
+        type = 1
+
+
+    return type
+
+def main():
+    bmi = calculate_bmi(weight=70, height=1.75)
+    print(bmi)
+
+if __name__ == "__main__":
+    main()
+
